f0a683b1b835a752846918c1604a509768013bb4
Candywolf <candywolf11+web@gmail.com>
2022-09-30 00:04:20 +0200

    style: hun typo

--- src/Data.tsx
+++ src/Data.tsx
@@ -3,24 +3,24 @@ import { HeaderProps } from './layout/Header';
 
 export const headerProps: HeaderProps = {
   title: 'Relax Time',
-  subtitle: 'Szépség es egészség',
+  subtitle: 'Szépség és egészség',
 };
 
 export const cards: CardProps[] = [
   {
-    title: 'Svedmasszazs',
+    title: 'Svédmasszázs',
     image: '/massage/massage-sved.jpg',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
   {
-    title: 'Relaxacios Masszazs',
+    title: 'Relaxációs Masszázs',
     image: '/massage/massage-relax.jpg',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
   {
-    title: 'Talpmasszazs',
+    title: 'Talpmasszázs',
     image: '/massage/massage-talp.jpg',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
